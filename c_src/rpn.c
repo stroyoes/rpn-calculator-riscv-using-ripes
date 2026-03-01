5947c9e634458c643db5a94e5e00f58d5f66b193
stroyoes <mazky7804@gmail.com>
2026-03-01 21:05:10 +0530

    feat(add-funcs)( ⚡ ): Add the is_operator and is_space predicate funcs too

--- c_src/rpn.c
+++ c_src/rpn.c
@@ -16,8 +16,17 @@ static int pop(void) {
   return stack[--top];
 }
 
-// To make sure the char is a digit 
+// Checks if the char is a digit 
 bool is_digit(char c) {
   return c >= '0' && c <= '9';
 }
 
+// Checks if its an operator 
+bool is_operator(char c) {
+  return c == '+' || c == '-' || c == '*' || c == '/';
+}
+
+// Checks if its a space
+bool is_space(char c) {
+  return c == ' ' || c == '\t' || c == '\n' || c == '\v' || c == '\f' || c == '\r';
+}
